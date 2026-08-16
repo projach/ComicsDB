d88b7c33408eb70c37950b4bdc72833e81b9ce8a
projach <ichaliasas@gmail.com>
2026-08-16 19:59:46 +0300

    remove passlib

--- core/auth.py
+++ core/auth.py
@@ -2,10 +2,10 @@ import os
 from datetime import datetime, timedelta, timezone
 
 import asyncpg
+import bcrypt
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
-from passlib.context import CryptContext
 
 from users.crud import get_user_by_username
 
@@ -14,16 +14,15 @@ from .database import get_db
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE = 7
+ENCODING = "utf-8"
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    return bcrypt.hashpw(password.encode(ENCODING), bcrypt.gensalt()).decode(ENCODING)
 
 def verify_password(plain: str, hashed: str) -> bool:
-    return pwd_context.verify(plain, hashed)
+    return bcrypt.checkpw(plain.encode(ENCODING), hashed.encode(ENCODING))
 
 def create_access_token(data: dict) -> str:
     to_encode = data.copy()
