eede38b7e9f22e0116d9bcdf32a7bf21f07d87f8
Iakovos Chaliasas <34036272+projach@users.noreply.github.com>
2026-09-14 02:04:00 +0300

    Bump version to 0.0.1

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from comics.router import router as comics_router
 from core.database import lifespan
 from fastapi import FastAPI
 from users.router import router as users_router
+import __version__
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan, version=__version__)
 
 app.include_router(comics_router)
 app.include_router(users_router)
