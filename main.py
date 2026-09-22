d8d074952a518f14d1ab8235e64ddab7a19c3ed1
projach <ichaliasas@gmail.com>
2026-09-22 23:30:13 +0300

    add awake endpoint

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from comics.router import router as comics_router
 from core.database import lifespan
 from fastapi import FastAPI
 from users.router import router as users_router
+from health.router import router as health_router
 
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(comics_router)
 app.include_router(users_router)
+app.include_router(health_router)
